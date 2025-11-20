f8c3884a635cfbce89b80664cb59541200313f37
wangmander <153228408+wangmander@users.noreply.github.com>
2025-11-20 14:21:48 -0800

    Update page.tsx

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,14 +1,11 @@
 "use client"
 
-import { SessionProvider } from "next-auth/react"
 import { GoogleConnectionCard } from "@/components/GoogleConnectionCard"
 
 export default function SettingsPage() {
   return (
-    <SessionProvider>
-      <div className="max-w-3xl mx-auto p-6">
-        <GoogleConnectionCard />
-      </div>
-    </SessionProvider>
+    <div className="max-w-3xl mx-auto p-6">
+      <GoogleConnectionCard />
+    </div>
   )
 }
