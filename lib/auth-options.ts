4f9887541a52e3def035d48318ce6021e5edd0da
wangmander <153228408+wangmander@users.noreply.github.com>
2025-11-20 15:19:31 -0800

    Update auth-options.ts

--- lib/auth-options.ts
+++ lib/auth-options.ts
@@ -1,1 +1,22 @@
+import GoogleProvider from "next-auth/providers/google";
+import type { NextAuthOptions } from "next-auth";
 
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          scope:
+            "openid email profile https://www.googleapis.com/auth/gmail.readonly",
+          access_type: "offline",
+          prompt: "consent"
+        }
+      }
+    })
+  ],
+  session: {
+    strategy: "jwt"
+  }
+};
