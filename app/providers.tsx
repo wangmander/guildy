a8271e19a71b0be1de358e65edb6bb5be8ddaee2
wangmander <153228408+wangmander@users.noreply.github.com>
2025-11-20 12:24:09 -0800

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,8 +1,7 @@
-"use client"
+"use client";
 
-import { SessionProvider } from "next-auth/react"
+import { SessionProvider } from "next-auth/react";
 
-export default function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>
+export function Providers({ children }: { children: React.ReactNode }) {
+  return <SessionProvider>{children}</SessionProvider>;
 }
-
